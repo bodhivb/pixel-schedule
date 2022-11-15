7e4559d1004828b0b31b7367bd678dae070789ad
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2022-11-15 16:58:30 +0100

    Add teacher

--- source/Views/schoolView.ts
+++ source/Views/schoolView.ts
@@ -1,8 +1,14 @@
 import { Texture } from "pixi.js";
+import { Teacher } from "../objects/teacher";
 import { View } from "./view";
 
 export class SchoolView extends View {
   constructor() {
     super("School");
+
+    let bodhi = new Teacher(Texture.from("assets/teachers/bodhi.png"));
+
+    this.entities.push(bodhi);
+    this.addChild(bodhi);
   }
 }
---
+++ source/objects/teacher.ts
@@ -0,0 +1,19 @@
+import { Resource, Texture } from "pixi.js";
+import { Entity } from "./entity";
+
+export class Teacher extends Entity {
+  constructor(texture?: Texture<Resource> | undefined) {
+    super(texture);
+
+    this.anchor.set(0.5, 0.9);
+
+    this.x = 400;
+    this.y = 400;
+
+    this.scale.set(2);
+  }
+
+  public Update(dt: number): void {
+    this.x += 3 * dt;
+  }
+}
