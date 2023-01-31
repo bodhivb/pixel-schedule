a054f7408a7129540b8cc570d08c74223450cd54
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2023-01-31 16:55:58 +0100

    Add room class

--- source/objects/room.ts
+++ source/objects/room.ts
@@ -1,17 +1,21 @@
-import { Sprite, Texture } from "pixi.js";
+import { Cache, Sprite, Texture } from "pixi.js";
 import { IRoom } from "../interfaces/roomInterface";
 import { SortingLayer } from "../interfaces/sortingLayerEnum";
 
-/*
 export class Room extends Sprite {
-  constructor(data: IRoom, xAxis: number) {
-    super(Texture.from("assets/" + data.type.url));
+  /** Room name */
+  public name: string;
+  /** Room number */
+  public number: string;
 
+  constructor(data: IRoom) {
+    super(Cache.get(data.type.asset));
+
+    this.number = data.number;
+    this.name = data.type.name;
+
+    // Default settings for room
     this.anchor.set(0, 1);
     this.zIndex = SortingLayer.Room;
-
-    this.x = 170 * xAxis + 200;
-    this.y = 400;
   }
 }
-*/
