9d308cc686779d5d7b5a0487fffb82df14f5b8af
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2023-01-31 16:56:28 +0100

    Fix naming

--- source/interfaces/roomType.ts
+++ source/interfaces/roomType.ts
@@ -1,11 +1,11 @@
 import { IRoomType } from "./roomTypeInterface";
 
 export const RoomType: { [key: string]: IRoomType } = {
-  classroom: { name: "Instructie", assets: "classroom" },
-  classroom_window: { name: "Instructie", assets: "classroom_with_windows" },
-  classroom_Large: { name: "Instructie", assets: "classroom_large" },
-  networking_plaza: { name: "Netwerkplein", assets: "classroom_network" },
-  conversation_booth: { name: "Gespreksruimte", assets: "workplace" },
-  office: { name: "Docentenruimte", assets: "office" },
-  elevator: { name: "Liftkamer", assets: "liftkamer" },
+  classroom: { name: "Instructie", asset: "classroom" },
+  classroom_window: { name: "Instructie", asset: "classroom_with_windows" },
+  classroom_Large: { name: "Instructie", asset: "classroom_large" },
+  networking_plaza: { name: "Netwerkplein", asset: "classroom_network" },
+  conversation_booth: { name: "Gespreksruimte", asset: "workplace" },
+  office: { name: "Docentenruimte", asset: "office" },
+  elevator: { name: "Liftkamer", asset: "liftkamer" },
 };
--- source/interfaces/roomTypeInterface.ts
+++ source/interfaces/roomTypeInterface.ts
@@ -1,4 +1,4 @@
 export interface IRoomType {
   name: string;
-  assets: string;
+  asset: string;
 }
