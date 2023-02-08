02ae4b6c91cbf72674551d6d788c3dad61f56c2c
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2023-02-08 13:06:46 +0100

    Update webpack.config.js
    Add ts check

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,60 @@
+// @ts-check
 const path = require("path");
 
-// Plugins
+// Webpack plugins
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
 
-// Default config
-var config = {
+/**
+ * Html-head options
+ * @type { HtmlWebpackPlugin.Options }
+ */
+const htmlOptions = {
+  title: "Pixel Schedule",
+  meta: {
+    description: "An interactive SintLucas pixel schedule.",
+    "og:title": { property: "og:title", content: "Pixel Schedule" },
+    "og:type": { property: "og:type", content: "website" },
+    "og:url": {
+      property: "og:url",
+      content: "https://bodhivb.github.io/pixel-schedule",
+    },
+    "og:image": {
+      property: "og:image",
+      content:
+        "https://bodhivb.github.io/pixel-schedule/assets/outdoors/tree.png",
+    },
+    "og:description": {
+      property: "og:description",
+      content: "An interactive SintLucas pixel schedule.",
+    },
+  },
+};
+
+/**
+ * Default config
+ * @type { import('webpack').Configuration }
+ */
+const config = {
   entry: "./source/index.ts",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [{ test: /\.tsx?$/, use: "ts-loader" }],
-  },
+  resolve: { extensions: [".tsx", ".ts", ".js"] },
+  module: { rules: [{ test: /\.tsx?$/, use: "ts-loader" }] },
   plugins: [
     new CopyWebpackPlugin({ patterns: [{ from: "assets/", to: "assets/" }] }),
-    new HtmlWebpackPlugin({
-      title: "Pixel Schedule",
-      meta: {
-        description: "An interactive SintLucas pixel schedule.",
-        "og:title": { property: "og:title", content: "Pixel Schedule" },
-        "og:type": { property: "og:type", content: "website" },
-        "og:url": {
-          property: "og:url",
-          content: "https://bodhivb.github.io/pixel-schedule/",
-        },
-        "og:image": {
-          property: "og:image",
-          content:
-            "https://bodhivb.github.io/pixel-schedule/assets/outdoors/tree.png",
-        },
-        "og:description": {
-          property: "og:description",
-          content: "An interactive SintLucas pixel schedule.",
-        },
-      },
-    }),
+    new HtmlWebpackPlugin(htmlOptions),
     new HtmlWebpackTagsPlugin({
       tags: ["assets/css/base.css", "assets/css/elements.css"],
     }),
   ],
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+  output: { filename: "main.js", path: path.resolve(__dirname, "dist") },
 };
 
-//Export config
+/**
+ * Export config
+ * @param {any} env
+ * @param {any} argv
+ */
 module.exports = (env, argv) => {
   config.mode = argv.mode;
 
