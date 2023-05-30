11fdd814f831b4ed4dafa7cafbb1a1a5d0ae7c75
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2023-05-30 14:18:28 +0200

    Update searchListComponent.ts

--- source/components/searchListComponent.ts
+++ source/components/searchListComponent.ts
@@ -6,6 +6,7 @@ import { teacherStore } from "../store/teacherStore";
 // SearchList component
 export class SearchListComponent {
   private box: HTMLElement;
+  private items: HTMLDivElement[] = [];
 
   public get view(): HTMLElement {
     return this.box;
@@ -21,9 +22,17 @@ export class SearchListComponent {
   }
 
   private loadTeacherData() {
+    // Clear the box first
+    for (let i = this.items.length - 1; i >= 0; i--) {
+      this.box.removeChild(this.items[i]);
+    }
+    this.items = [];
+
+    // Load the teacher data
     for (let teacher of teacherStore.GetData()) {
       const teacherBox = createTeacherBox(teacher);
       this.box.appendChild(teacherBox);
+      this.items.push(teacherBox);
     }
   }
 }
